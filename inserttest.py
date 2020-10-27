ff743820aa07358b0d3ead3ad8779602b2444181
nicklesimba <simha.nikhil@gmail.com>
2020-10-27 18:03:41 -0500

    managed to insert records into db

--- inserttest.py
+++ inserttest.py
@@ -17,12 +17,13 @@ print(cursor)
 
 sql = """
     INSERT INTO Users (username, locations, prof_pic_url, password) 
-    VALUES ("nicklesimba", "Champaign", "haskellcurry.com", "nikhils_password"), ("rammk1999", "Champaign", "ram.com", "rams_password")
+    VALUES ("nicklesimba", "Urbana", "nikhil.com", "nikhils_pass"), ("rammk1999", "Champaign", "ram.com", "rams_password"), ("maanuSort", "Champaign", "mondo.com", "fakemaanus_password")
 """
 
 number_of_rows = cursor.execute(sql)
 
 print(number_of_rows)
 
+db.commit()
 db.close()
 
