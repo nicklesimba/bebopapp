14b3bc613eff4ea7a83b6dbdd2d2188f86173ea8
RamMuthukumaran <rammk1999@gmail.com>
2020-10-27 20:28:48 -0500

    Added more USers and created a post

--- inserttest.py
+++ inserttest.py
@@ -17,12 +17,17 @@ print(cursor)
 
 sql = """
     INSERT INTO Users (username, locations, prof_pic_url, password) 
-    VALUES ("nicklesimba", "Urbana", "nikhil.com", "nikhils_pass"), ("rammk1999", "Champaign", "ram.com", "rams_password"), ("maanuSort", "Champaign", "mondo.com", "fakemaanus_password")
+    VALUES ("nicklesimba", "Urbana", "nikhil.com", "nikhils_password"), ("rammk1999", "Champaign", "ram.com", "rams_password"), ("MondoLongwell", "Champaign", "mondo.com", "maanus_password"), ("DivsB", "Champaign", "divyabhati.com", "divyas_password")
 """
 
 number_of_rows = cursor.execute(sql)
 
 print(number_of_rows)
+
+sql = """
+    INSERT INTO Posts (post_id, location, post_message, reply_ids, likes, dislikes)
+    VALUES (1, "Champaign", "This is the 1st message put into the application", "", 0, 0)
+"""
 
 db.commit()
 db.close()
