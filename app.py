4cf8cd0ba580f6cb242c374b1994a1d6894bef30
RamMuthukumaran <rammk1999@gmail.com>
2020-10-29 20:33:06 -0500

    Creating login page

--- app.py
+++ app.py
@@ -3,10 +3,20 @@ import os
 from flask import Flask, request, render_template, redirect, url_for
 
 project_root = os.path.dirname(os.path.realpath('__file__'))
-template_path = os.path.join(project_root, 'app/templates')
-static_path = os.path.join(project_root, 'app/static')
+template_path = os.path.join(project_root, 'templates')
+static_path = os.path.join(project_root, 'static')
 app = Flask(__name__, template_folder=template_path, static_folder=static_path)
 
 @app.route('/')
 def index():
     return ' test world!'
+    
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    username = request.form['username']
+    password = request.form['password']
+    location = request.form['location']
+    
+    return render_template('login.html', error=error)
