c27b9e5f96a363868d4a96b4cd8ae3105feca117
Mondo Longwell <19lion@gmail.com>
2020-11-02 07:25:06 -0600

    implemented two Users table interaction functions
    needs testing

---
+++ dbio/SQL/users.py
@@ -0,0 +1,85 @@
+import mysql.connector as connector
+
+db = connector.connect(
+    host="localhost",
+    database="bebop26dmnr_db", 
+    user="bebop26dmnr_nicklesimba", 
+    password="Yareyaredaze2643"  
+)
+
+USERNAME_LENGTH = 100
+LOCATIONS_LENGTH = 255
+PROF_PIC_URL_LENGTH = 255
+PASSWORD_LENGTH = 255
+
+createQuery = """
+    INSERT INTO Users (username, locations, prof_pic_url, password)
+        VALUES (%s, %s, %s, %s);
+"""
+
+checkNameQuery = """
+    SELECT username
+    FROM Users
+    WHERE username = %s
+"""
+
+def create_user(username, password, location, prof_pic_url=""): # may need more fields later
+    """
+    Create a new user to store in the Users table.
+    :param string username: Username login of new user being created.
+    :param string password: Password to check on login of this user.
+    :param string location: Starting location of user.
+    :param string prof_pic_url: Profile picture link.
+    :return: 
+    """
+
+    if (len(username) > USERNAME_LENGTH):
+        errStr = "Username exceeds " + str(USERNAME_LENGTH) + " characters."
+        raise TypeError(errStr)
+    elif (len(password)):
+        errStr = "Password exceeds " + str(PASSWORD_LENGTH) + " characters."
+        raise TypeError(errStr)
+    elif (len(location)):
+        errStr = "Location exceeds " + str(LOCATIONS_LENGTH) + " characters."
+        raise TypeError(errStr)
+    elif (len(prof_pic_url)):
+        errStr = "Profile picture URL exceeds " + str(PROF_PIC_URL_LENGTH) + " characters."
+        raise TypeError(errStr)
+
+    cursor = db.cursor(prepared=True)
+    numRows = cursor.execute(checkNameQuery, (username))
+    cursor.close()
+
+    if (numRows > 0):
+        raise RuntimeError("Username taken.")
+    else:
+        cursor = db.cursor(prepared=True)
+        cursor.execute(createQuery, (username, location, prof_pic_url, password))
+        cursor.close()
+        
+
+
+searchQuery = """
+    SELECT username
+    FROM Users
+    WHERE username = %s AND password = %s
+"""
+
+def check_login(username, password):
+    """
+    Check the login info by querying the database for the provided username and password.
+    :param string username: Username of login to check.
+    :param string password: Password of login to check.
+    :return bool: True, if username-password combination exists in database,
+            False if either does not
+    """
+
+    cursor = db.cursor(prepared=True)
+
+    numRows = cursor.execute(searchQuery, (username, password))
+    cursor.close()
+
+    if (numRows == 1):
+        True
+    else:
+        False
