d41db1902d66cd1deebbb22e0e81c7b4a9965326
RamMuthukumaran <rammk1999@gmail.com>
2020-11-03 15:54:12 -0600

    Verified that the front end works

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # app.py
 import os
 from flask import Flask, request, render_template, redirect, url_for
-#import dbio.SQL.users.py as users
+import dbio.SQL.users.py as users
 
 project_root = os.path.dirname(os.path.realpath('__file__'))
 template_path = os.path.join(project_root, 'templates')
@@ -31,14 +31,14 @@ def login():
             return render_template('login.html', error=error)
             
         else:
-            error = "Invalid Username and Password"
+            error = "Invalid Username or Password"
             return render_template('login.html', error=error)
         
     elif 'Register' in request.form:
         try:
             users.create_user(username, password, location)
         except (TypeError, RuntimeError) as e:
-            error = e.args
+            error = e.args[0]
             return render_template('login.html', error=error)
             
     else:
