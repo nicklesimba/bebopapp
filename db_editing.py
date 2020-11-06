5810b81ce06c3b7cc4a5df2424ca752412c3c95d
RamMuthukumaran <rammk1999@gmail.com>
2020-11-05 23:52:14 -0600

    changed my.connect

--- db_editing.py
+++ db_editing.py
@@ -3,11 +3,12 @@ from __future__ import print_function
 import mysql.connector as my
 
 db = my.connect(
-    host="localhost", 
-    user="bebop26dmnr_nicklesimba", 
-    passwd="Yareyaredaze2643", 
-    db="bebop26dmnr_db" 
+        host="localhost",
+        database="bebop26dmnr_db", 
+        user="bebop26dmnr_nicklesimba", 
+        password="Yareyaredaze2643"  
 )
+
 cursor = db.cursor()
 
 clear_post_interacts = """
